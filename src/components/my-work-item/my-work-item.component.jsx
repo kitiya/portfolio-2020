11bfe60c955383e31af72efdbe6f32dcb0165f80
Kitiya <kate.kitiya@gmail.com>
2020-04-27 08:31:02 -0400

    MyWorkItem component: responsive ui

--- src/components/my-work-item/my-work-item.component.jsx
+++ src/components/my-work-item/my-work-item.component.jsx
@@ -9,39 +9,32 @@ const MyWorkItem = ({ title, techStack, description, imageUrl }) => {
     <Fade>
       <section className="work-item">
         <h2 className="work-item__title">{title}</h2>
-        <div className="work-item__main-content">
-          <div className="container container__image">
-            <img src={urlPath} alt="" className="work-item__image" />
+        <img src={urlPath} alt="" className="work-item__image" />
+
+        <main className="work-item__main">
+          <div className="work-item__text-content">
+            <p className="work-item__description">{description}</p>
             <div className="work-item__actions">
-              <a className="btn btn__mywork btn__mywork--demo" href="/#">
+              <a className="btn btn__mywork" href="/#">
                 Live Project
               </a>
-              <a
-                className="btn  btn__mywork btn__mywork--source-code"
-                href="/#"
-              >
+              <a className="btn  btn__mywork" href="/#">
                 Source Code
               </a>
             </div>
           </div>
-          <div className="container container__text-content">
-            <p className="work-item__description">{description}</p>
-          </div>
-        </div>
-        <div className="work-item__footer">
-          <h4 className="tech-stack__subtitle">Tech Stack</h4>
-          <div className="tech-stack">
+          <div className="work-item__tech-stack">
             {techStack.map((item, index) => (
               <img
                 key={index}
                 src={`${iconUrl}/${item.icon}.svg`}
                 alt={item.title}
-                className="work-item__brand-icon"
+                className="tech-stack__brand-icon"
                 title={item.title}
               />
             ))}
           </div>
-        </div>
+        </main>
       </section>
     </Fade>
   );
